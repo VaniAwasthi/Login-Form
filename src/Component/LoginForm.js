852c9bb780516a2952ab28822f1ae4e82cda8daa
Vani Awasthi <awasthivani95@gmail.com>
2023-03-30 22:54:16 +0530

    Adding last change  existing project files to Git

--- src/Component/LoginForm.js
+++ src/Component/LoginForm.js
@@ -2,12 +2,23 @@ import  "./LoginForm.css"
 
 
 const LoginForm=(props)=>{
+    const [focused , setFocused]=useState(false)
     const {label,errorMessage, onChange,id,...inputProps}=props
+    
+    const handleFocus=(e)=>{
+        setFocused(true);
+    }
+    
     return(
 
         <div className="loginForm">
             <label>{label}</label> 
-            <input{...inputProps} onChange={onChange} />
+            <input{...inputProps}
+                onChange={onChange}
+                onBlur={handleFocus} 
+                onFocus={()=>
+                inputProps.name==="confirPassword" && setFocused(true)}
+                focused={focused.toString}/>
             <span>{errorMessage}</span>
         </div>
     )
